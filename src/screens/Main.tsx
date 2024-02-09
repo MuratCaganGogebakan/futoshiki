43dba6a9d93ce1bc8a49eeaf670c891d7cdb7b2d
Murat Göğebakan <36950001+MuratCaganGogebakan@users.noreply.github.com>
2024-02-09 14:26:56 +0000

    feat: use a real game as dummy

--- src/screens/Main.tsx
+++ src/screens/Main.tsx
@@ -1,32 +1,37 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { Constraint, FutoshikiGrid } from '../components/Main/FutoshikiGrid'
+import React from "react";
+import { View } from "react-native";
+
+import { Constraint, FutoshikiGrid } from "../components/Main/FutoshikiGrid";
 
 const dummyData = {
   grid: [
-    [5, 0, 0, 0, 0],
-    [0, 0, 3, 0, 0],
-    [0, 4, 0, 0, 0],
-    [0, 0, 0, 2, 0],
-    [0, 0, 0, 0, 1]
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
   ],
   constraints: [
-    { cell1: [0, 0], cell2: [1, 0] },
-    { cell1: [1, 2], cell2: [1, 3] },
-    { cell1: [2, 1], cell2: [2, 2] },
-    { cell1: [3, 3], cell2: [3, 4] },
-    { cell1: [4, 3], cell2: [4, 4] },
-    { cell1: [1, 0], cell2: [2, 0] },
-    { cell1: [2, 2], cell2: [3, 2] },
-    { cell1: [3, 1], cell2: [4, 1] }
-  ] as Constraint[]
-}
+    { cell1: [0, 0], cell2: [0, 1] },
+    { cell1: [0, 1], cell2: [0, 2] },
+    { cell1: [0, 2], cell2: [0, 3] },
+    { cell1: [0, 4], cell2: [1, 4] },
+    { cell1: [1, 4], cell2: [1, 3] },
+    { cell1: [1, 1], cell2: [1, 0] },
+    { cell1: [2, 4], cell2: [1, 4] },
+    { cell1: [2, 3], cell2: [2, 4] },
+    { cell1: [3, 3], cell2: [3, 2] },
+    { cell1: [4, 4], cell2: [3, 4] },
+  ] as Constraint[],
+};
 
 export const Main = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <FutoshikiGrid initialGrid={dummyData.grid} constraints={dummyData.constraints} />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <FutoshikiGrid
+        initialGrid={dummyData.grid}
+        constraints={dummyData.constraints}
+      />
     </View>
-  )
-}
+  );
+};
